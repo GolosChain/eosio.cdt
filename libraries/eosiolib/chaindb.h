a504653231f334695fe8db6e71508a18d98a2349
e-schepachev <44019320+e-schepachev@users.noreply.github.com>
2018-12-29 14:22:19 +0700

    fix broken multi_index::find  (#21)
    fix broken multi_index::find #20

--- libraries/eosiolib/chaindb.h
+++ libraries/eosiolib/chaindb.h
@@ -17,6 +17,8 @@ typedef uint64_t primary_key_t;
 static constexpr primary_key_t end_primary_key = static_cast<primary_key_t>(-1);
 static constexpr primary_key_t unset_primary_key = static_cast<primary_key_t>(-2);
 
+cursor_t chaindb_opt_find_by_pk(account_name_t code, scope_t scope, table_name_t table, primary_key_t pk);
+
 cursor_t chaindb_lower_bound(account_name_t code, scope_t scope, table_name_t, index_name_t, void* key, size_t);
 cursor_t chaindb_upper_bound(account_name_t code, scope_t scope, table_name_t, index_name_t, void* key, size_t);
 cursor_t chaindb_find(account_name_t code, scope_t scope, table_name_t, index_name_t, primary_key_t, void* key, size_t);
