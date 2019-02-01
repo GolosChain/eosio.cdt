878eeba04ef1f774d29d2a4e06ad9c37f51a8ae0
Evgeny Schepachev <schepachev@gmail.com>
2019-02-01 17:56:11 +0700

    removed duplicated second.pw smart contracts #33

--- examples/request.bw/second.pw.cpp
+++ examples/request.bw/second.pw.cpp
@@ -2,9 +2,16 @@
 #include <eosiolib/print.hpp>
 #include <eosiolib/bandwith.hpp>
 
-class [[eosio::contract]] second_provider : public eosio::contract {
+class [[eosio::contract]] second_pw : public eosio::contract {
 public:
-  second_provider( eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds) : eosio::contract(receiver, code, ds) {}
+  second_pw( eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds) : eosio::contract(receiver, code, ds) {}
+
+  [[eosio::action]] void verify(eosio::name account) {
+        eosio::print("The verify action of secondary provider is called\n");
+
+        eosio::print("      net limit: ", get_bw_net_limit(account), "\' cpu limit  \'", get_bw_cpu_limit(account), "\'\n");
+        eosio::print("      is confirmed: ", is_provided_bw_confirmed(account), "\n");
+  }
 
   [[eosio::action]] void approvebw(eosio::name account) {
         eosio::print("Secondary provider\n");
@@ -13,6 +20,10 @@ public:
         eosio::print("      is confirmed \'", is_provided_bw_confirmed(account), "\'\n");
 
         set_bw_limits(account, 200, 200);
+
+#ifdef FAIL_CASE
+        confirm_bw_limits(account); //due to confirmation in this action. Transaction have to fail when the next provider tries to set or confirm bandwith
+#endif
 
         eosio::print("  After set_bw_limit\n");
 
@@ -27,9 +38,20 @@ public:
             "approvebw"_n,
             account
         ).send();
+
+#ifdef VERIFIED_CASE
+
+        eosio::action(
+            eosio::permission_level{get_self(), "active"_n},
+            get_self(),
+            "verify"_n,
+            account
+        ).send();
+
+#endif
   }
 
 private:
 };
 
-EOSIO_DISPATCH( second_provider, (approvebw))
+EOSIO_DISPATCH( second_pw, (approvebw)(verify) )
