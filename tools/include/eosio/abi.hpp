9bf5230a924e4c939d275ae437e9378676db9071
zxcat <550974+zxcat@users.noreply.github.com>
2019-02-06 17:49:22 +0300

    Remove code related to ricardian contracts #42

--- tools/include/eosio/abi.hpp
+++ tools/include/eosio/abi.hpp
@@ -26,7 +26,6 @@ struct abi_struct {
 struct abi_action {
    std::string name;
    std::string type;
-   std::string ricardian_contract;
    bool operator<(const abi_action& s) const { return name < s.name; }
 };
 
@@ -37,11 +36,6 @@ struct abi_table {
    std::vector<std::string> key_names;
    std::vector<std::string> key_types;
    bool operator<(const abi_table& t) const { return name < t.name; }
-};
-
-struct abi_ricardian_clause_pair {
-   std::string id;
-   std::string body;
 };
 
 struct abi_variant {
@@ -57,13 +51,12 @@ struct abi_error_message {
 
 /// From eosio libraries/chain/include/eosio/chain/abi_def.hpp
 struct abi {
-   std::string version = "eosio::abi/1.1";
+   std::string version = "cyberway::abi/1.1";
    std::set<abi_struct>  structs;
    std::set<abi_typedef> typedefs;
    std::set<abi_action>  actions;
    std::set<abi_table>   tables;
    std::set<abi_variant> variants;
-   std::vector<abi_ricardian_clause_pair>   ricardian_clauses;
    std::vector<abi_error_message> error_messages;
 };
 
